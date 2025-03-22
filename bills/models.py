aecd04b93de0fead7040b2e2cee0497be494090b
Trish Duce <ducepa@mso.umt.edu>
2025-03-22 12:58:01 -0600

    css updates

--- bills/models.py
+++ bills/models.py
@@ -4,11 +4,11 @@ from django.db import models
 # Create your models here.
 class Bill(models.Model):
     name = models.CharField(max_length=255)
-    phone = models.CharField(max_length=20)
-    website = models.CharField(max_length=20)
-    account_number = models.CharField(max_length=20)
+    phone = models.CharField(max_length=20, null=True, blank=True)
+    website = models.CharField(max_length=255, null=True, blank=True)
+    account_number = models.CharField(max_length=30, null=True, blank=True)
     autopay = models.BooleanField(default=False)
-    username = models.CharField(max_length=20)
+    username = models.CharField(max_length=30, null=True, blank=True)
 
     def __str__(self):
         return self.name
--- contacts/models.py
+++ contacts/models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 class Contact(models.Model):
     business = models.CharField(max_length=255)
-    bus_phone = models.CharField(max_length=20)
-    contact = models.CharField(max_length=255)
-    cont_phone = models.CharField(max_length=20)
+    bus_phone = models.CharField(max_length=20, null=True, blank=True)
+    contact = models.CharField(max_length=255, null=True, blank=True)
+    cont_phone = models.CharField(max_length=20, null=True, blank=True)
     
     def __str__(self):
         return self.business
