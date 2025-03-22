9e5634e64874c6df848fb08acb539812907f76df
Trish Duce <ducepa@mso.umt.edu>
2025-03-21 20:05:08 -0600

    home page link error fix

--- django_project/settings.py
+++ django_project/settings.py
@@ -141,7 +141,6 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 AUTH_USER_MODEL = "accounts.CustomUser"
 
-LOGIN_URL = '/login/'
 LOGIN_REDIRECT_URL = "home" 
 LOGOUT_REDIRECT_URL = "home" 
 
--- maintenance/urls.py
+++ maintenance/urls.py
@@ -4,10 +4,10 @@ from .views import MaintenanceListView, MaintenanceDetailView, MaintenanceUpdate
 
 # Create your views here.
 urlpatterns = [
-    path('', HomeView.as_view(), name='home'),
-    path('/maintenance/', MaintenanceListView.as_view(), name='maintenance_list'),
+    path("", HomeView.as_view(), name='home'),
+    path('maintenancelist/', MaintenanceListView.as_view(), name='maintenance_list'),
     path('<int:pk>/', MaintenanceDetailView.as_view(), name='maintenance_detail'),
     path('<int:pk>/edit/', MaintenanceUpdateView.as_view(), name='maintenance_edit'),
-    path('/new/', MaintenanceCreateView.as_view(), name='maintenance_new'),
+    path('new/', MaintenanceCreateView.as_view(), name='maintenance_new'),
     path('<int:pk>/delete/', MaintenanceDeleteView.as_view(), name='maintenance_delete'),
 ]
